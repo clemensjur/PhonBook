962d20264031b60f725139a2e328d64b982079a8
Clemens Jurkowitsch <clemens.jurkowitsch@gmx.at>
2021-10-08 16:11:56 +0200

    [INFO] Initial Commit

---
+++ backend/src/main/java/at/clemensjur/phonbook/PhonBookApplication.java
@@ -0,0 +1,13 @@
+package at.clemensjur.phonbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhonBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhonBookApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/at/clemensjur/phonbook/PhonBookApplicationTests.java
@@ -0,0 +1,13 @@
+package at.clemensjur.phonbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhonBookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
